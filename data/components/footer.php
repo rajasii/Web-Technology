a4fa19215cd9b68d51ca28a9c9cff071e7576337
rajasii <152724304+rajasii@users.noreply.github.com>
2024-07-13 19:18:25 +0530

    Add files via upload

---
+++ data/components/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+
+   <section class="grid">
+
+     
+
+      <div class="box" style="padding-left: 50px">
+         <h3>contact us</h3>
+         <a ><i class="fas fa-phone"></i>+91 9898989898</a>
+         <a><i class="fas fa-phone"></i> +91 8769876543</a>
+         <a><i class="fas fa-map-marker-alt"></i> Kothrud, Pune-411038</a>
+      </div>
+
+   
+
+   </section>
+
+</footer>
